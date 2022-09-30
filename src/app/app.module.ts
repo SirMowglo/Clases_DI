fd075592a48b02495ce8a1fa993349b32b1a3605
Alejandro Damas <alejandrodamas3d@gmail.com>
2022-09-30 09:56:22 +0200

    Login UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialImportsModule } from './material-imports/material-imports.module';
+import { E01BindingComponent } from './e01-binding/e01-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    E01BindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/e01-binding/e01-binding.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-e01-binding',
+  templateUrl: './e01-binding.component.html',
+  styleUrls: ['./e01-binding.component.css']
+})
+export class E01BindingComponent implements OnInit {
+
+  hide = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
